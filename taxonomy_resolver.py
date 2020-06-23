2ce8542511b7cf078963ceedcc4c4b868354ca48
Fábio Madeira <fbiomadeira@gmail.com>
2020-06-23 12:33:59 +0100

    JD-636 Added basic CLI callers

---
+++ taxonomy_resolver.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+"""
+NCBI Taxonomy Resolver
+
+:copyright: (c) 2020.
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
+from taxonresolver.cli import cli
+
+if __name__ == '__main__':
+    cli()
