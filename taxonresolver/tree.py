40dfe09db3534a637e5948628dc70ccb21a9c451
Fábio Madeira <fbiomadeira@gmail.com>
2020-08-10 11:54:14 +0100

    JD-682 Reverted functionality to only use anytree - spliting the fast mode altogether

--- taxonresolver/tree.py
+++ taxonresolver/tree.py
@@ -9,7 +9,6 @@ NCBI Taxonomy Resolver
 """
 
 import io
-import json
 import copy
 import pickle
 import zipfile
@@ -101,22 +100,6 @@ def tree_reparenting(tree_dict: dict, root_key: str = "1",
     return tree_dict[root_key]
 
 
-def tree_reparenting_fast(tree_dict: dict) -> dict:
-    """
-    Loops over the Tree dictionary and reparents every node
-
-    :param tree_dict: dict of anytree node objects
-    :return: updated dict object
-    """
-    for k, v in tree_dict["nodes"].items():
-        if v["parent_tax_id"] not in tree_dict["parents"]:
-            tree_dict["parents"][v["parent_tax_id"]] = [v["tax_id"]]
-        else:
-            tree_dict["parents"][v["parent_tax_id"]].append(v["tax_id"])
-
-    return tree_dict
-
-
 def build_tree(inputfile: str, root_key: str = "1",
                logging: logging or None = None) -> Node:
     """
@@ -181,37 +164,13 @@ def build_tree(inputfile: str, root_key: str = "1",
     return tree_reparenting(taxon_nodes, root_key)
 
 
-def build_tree_fast(inputfile: str) -> dict:
-    """
-    Given the path to the taxdmp.zip file, build a full tree,
-    by converting nodes to anytree nodes.
-
-    :param inputfile: Path to taxdmp.zip file
-    :return: dict object
-    """
-
-    tree_dict = {"nodes": {}, "parents": {}}
-    with zipfile.ZipFile(inputfile) as taxdmp:
-        # read nodes
-        with taxdmp.open("nodes.dmp") as dmp:
-            for line in io.TextIOWrapper(dmp):
-                dict_node = {}
-                fields = split_line(line)
-                dict_node["tax_id"] = fields[0]
-                dict_node["parent_tax_id"] = fields[1]
-                dict_node["rank"] = label_to_id(fields[2])
-                tree_dict["nodes"][fields[0]] = dict_node
-    return tree_reparenting_fast(tree_dict)
-
-
 def load_tree(inputfile: str, inputformat: str = "pickle",
-              mode: str = "anytree", root_key: str = "1", **kwargs) -> Node:
+              root_key: str = "1", **kwargs) -> Node:
     """
     Loads pre-existing Tree from file.
 
     :param inputfile: Path to outputfile
     :param inputformat: "json" or "pickle"
-    :param mode: "anytree" or "fast"
     :param root_key: Key of the root Node
     :return: dict of anytree node objects
     """
@@ -219,25 +178,19 @@ def load_tree(inputfile: str, inputformat: str = "pickle",
     if inputformat == "pickle":
         return pickle.load(open(inputfile, "rb"))
     elif inputformat == "json":
-        if mode == "anytree":
-            importer = JsonImporter(**kwargs)
-            with open(inputfile) as data:
-                taxon_nodes = get_anytree_taxon_nodes(importer.read(data))
-                return tree_reparenting(taxon_nodes, root_key)
-        elif mode == "fast":
-            with open(inputfile) as data:
-                return json.load(data, **kwargs)
+        importer = JsonImporter(**kwargs)
+        with open(inputfile) as data:
+            taxon_nodes = get_anytree_taxon_nodes(importer.read(data))
+            return tree_reparenting(taxon_nodes, root_key)
 
 
-def write_tree(tree: Node, outputfile: str, outputformat: str,
-               mode: str = "anytree", **kwargs) -> None:
+def write_tree(tree: Node, outputfile: str, outputformat: str, **kwargs) -> None:
     """
     Writes Tree to file.
 
     :param tree: anytree Node object
     :param outputfile: Path to outputfile
     :param outputformat: "json" or "pickle"
-    :param mode: "anytree" or "fast"
     :return: (side-effects) writes to file
     """
 
@@ -245,13 +198,9 @@ def write_tree(tree: Node, outputfile: str, outputformat: str,
         with open(outputfile, 'wb') as outfile:
             pickle.dump(tree, outfile)
     elif outputformat == "json":
-        if mode == "anytree":
-            exporter = JsonExporter(**kwargs)
-            with open(outputfile, "w") as outfile:
-                exporter.write(tree, outfile)
-        elif mode == "fast":
-            with open(outputfile, 'w') as outfile:
-                json.dump(tree, outfile, **kwargs)
+        exporter = JsonExporter(**kwargs)
+        with open(outputfile, "w") as outfile:
+            exporter.write(tree, outfile)
 
 
 def filter_tree(tree: Node, filterfile: str, root_key: str = "1",
@@ -270,15 +219,17 @@ def filter_tree(tree: Node, filterfile: str, root_key: str = "1",
 
     tax_ids = parse_tax_ids(filterfile, sep, indx)
 
-    # get list of all required (and unique) tax_id parents
+    # get list of all required (and unique) parents and children taxIDs
     tax_id_parents = [node.name for tax_id in tax_ids
-                      for node in find(tree, filter_=lambda n: n.name == tax_id).path]
-    taxon_nodes = get_anytree_taxon_nodes(tree, filter_=lambda n: n.name in tax_id_parents)
+                      for leave in find(tree, filter_=lambda n:n.name == tax_id).leaves
+                      for node in find(tree, filter_=lambda n: n.name == leave.name).path]
+    taxon_nodes = get_anytree_taxon_nodes(tree,
+                                          filter_=lambda n: n.name in list(set(tax_id_parents)))
     return tree_reparenting(taxon_nodes, root_key)
 
 
 def search_tree(tree: Node, taxidfile: str, filterfile: str or None = None,
-                search_rank: str = "species", sep: str = " ", indx: int = 0) -> list:
+                sep: str = " ", indx: int = 0) -> list:
     """
     Searches an existing Tree and produces a list of TaxIDs.
     Checks if TaxID is in the list, if so provides as is, else,
@@ -288,7 +239,6 @@ def search_tree(tree: Node, taxidfile: str, filterfile: str or None = None,
     :param taxidfile: Path to file with TaxIDs to search with
     :param filterfile: Path to inputfile, which is a list of
         Taxonomy Identifiers (optional)
-    :param search_rank: Rank used for searching TaxIDs
     :param sep: separator for splitting the input file lines
     :param indx: index used for splicing the the resulting list
     :return: list of TaxIDs
@@ -301,48 +251,7 @@ def search_tree(tree: Node, taxidfile: str, filterfile: str or None = None,
 
     taxids_found = [tax_id for tax_id in taxids_search if tax_id in taxids_filter]
     taxids_found.extend([node.name for tax_id in taxids_search
-                         for node in find(tree, filter_=lambda n: n.name == tax_id).leaves
-                         if node.rank == search_rank])
-    return list(set(taxids_found))
-
-
-def search_tree_fast(tree: dict, taxidfile: str, filterfile: str or None = None,
-                     search_rank: str = "species", sep: str = " ", indx: int = 0) -> list:
-    """
-    Searches an existing Tree and produces a list of TaxIDs.
-    Checks if TaxID is in the list, if so provides as is, else,
-        searches all children in the list that compose that node.
-
-    :param tree: dict object
-    :param taxidfile: Path to file with TaxIDs to search with
-    :param filterfile: Path to inputfile, which is a list of
-        Taxonomy Identifiers (optional)
-    :param search_rank: Rank used for searching TaxIDs
-    :param sep: separator for splitting the input file lines
-    :param indx: index used for splicing the the resulting list
-    :return: list of TaxIDs
-    """
-
-    taxids_search = parse_tax_ids(taxidfile, sep, indx)
-    taxids_filter = []
-    if filterfile:
-        taxids_filter = parse_tax_ids(filterfile, sep, indx)
-
-    taxids_found = [tax_id for tax_id in taxids_search if tax_id in taxids_filter]
-    for tax_id in taxids_search:
-        # list of children
-        def get_leaves(taxids: list, tree: dict):
-            taxids_found.extend([t for t in taxids
-                                 if tree["nodes"][t]["rank"] == search_rank])
-            for taxid in taxids:
-                try:
-                    get_leaves(tree["parents"][taxid], tree)
-                except KeyError:
-                    # taxid is a leaf node - i.e. it is not a parent of any other TaxID
-                    pass
-
-        get_leaves(tree["parents"][tax_id], tree)
-
+                         for node in find(tree, filter_=lambda n: n.name == tax_id).leaves])
     return list(set(taxids_found))
 
 
@@ -354,18 +263,7 @@ def search_tree_by_taxid(tree: Node, tax_id: str) -> Node:
     :param tax_id: TaxID
     :return: anytree Node object
     """
-    return find(tree, filter_=lambda node: node.name == tax_id)
-
-
-def search_tree_by_taxid_fast(tree: dict, tax_id: str) -> Node:
-    """
-    Simply checks if TaxID is in the list or in the Tree.
-
-    :param tree: anytree Node object
-    :param tax_id: TaxID
-    :return: dict object
-    """
-    return tree["nodes"][tax_id]
+    return find(tree, filter_=lambda n: n.name == tax_id)
 
 
 def validate_tree(tree: Node, taxidfile: str, inputfile: str or None = None,
@@ -397,34 +295,6 @@ def validate_tree(tree: Node, taxidfile: str, inputfile: str or None = None,
     return False if False in taxids_valid else True
 
 
-def validate_tree_fast(tree: dict, taxidfile: str, inputfile: str or None = None,
-                       sep: str = " ", indx: int = 0) -> bool:
-    """
-    Simply checks if TaxID is in the list or in the Tree.
-
-    :param tree: dict object
-    :param taxidfile: Path to file with TaxIDs to search with
-    :param inputfile: Path to inputfile, which is a list of
-        Taxonomy Identifiers
-    :param sep: separator for splitting the input file lines
-    :param indx: index used for splicing the the resulting list
-    :return: boolean
-    """
-
-    taxids_search = parse_tax_ids(taxidfile, sep, indx)
-    taxids_filter = []
-    if inputfile:
-        taxids_filter = parse_tax_ids(inputfile, sep, indx)
-
-    taxids_valid = []
-    for tax_id in taxids_search:
-        if tax_id in taxids_filter or tax_id in tree["nodes"]:
-            taxids_valid.append(True)
-        else:
-            taxids_valid.append(False)
-    return False if False in taxids_valid else True
-
-
 def validate_tree_by_taxid(tree: Node, tax_id: str) -> bool:
     """
     Simply checks if TaxID is in the list or in the Tree.
@@ -433,31 +303,17 @@ def validate_tree_by_taxid(tree: Node, tax_id: str) -> bool:
     :param tax_id: TaxID
     :return: boolean
     """
-    return True if find(tree, filter_=lambda node: node.name == tax_id) else False
-
-
-def validate_tree_by_taxid_fast(tree: dict, tax_id: str) -> bool:
-    """
-    Simply checks if TaxID is in the list or in the Tree.
-
-    :param tree: dict object
-    :param tax_id: TaxID
-    :return: boolean
-    """
-    return tax_id in tree["nodes"]
+    return True if find(tree, filter_=lambda n: n.name == tax_id) else False
 
 
 class TaxonResolver(object):
-    def __init__(self, mode="anytree", logging=None, **kwargs):
+    def __init__(self, logging=None, **kwargs):
         self.root_key = "1"
         self.tree = None
         self._full_tree = None
         self.logging = logging
         self.kwargs = kwargs
-        self.mode = mode
         self._valid_formats = ("json", "pickle")
-        self._valid_modes = ("anytree", "fast")
-        self.search_rank = "species"
 
     def download(self, outputfile, outputformat) -> None:
         """Download NCBI Taxonomy dump file."""
@@ -466,104 +322,71 @@ class TaxonResolver(object):
 
     def build(self, inputfile) -> None:
         """Build a tree from NCBI dump file."""
-        if self.mode == "anytree":
-            self.tree = build_tree(inputfile, self.root_key, self.logging)
-            # keep a copy of the original (full) tree
-            self._full_tree = copy.copy(self.tree)
-        elif self.mode == "fast":
-            self.tree = build_tree_fast(inputfile)
+        self.tree = build_tree(inputfile, self.root_key, self.logging)
+        # keep a copy of the original (full) tree
+        self._full_tree = copy.copy(self.tree)
 
     def load(self, inputfile, inputformat) -> None:
         """Load a tree from JSON or Pickle files."""
         inputformat = inputformat.lower()
-        if inputformat in self._valid_formats and self.mode in self._valid_modes:
-            self.tree = load_tree(inputfile, inputformat,
-                                  self.mode, self.root_key, **self.kwargs)
+        if inputformat in self._valid_formats:
+            self.tree = load_tree(inputfile, inputformat, self.root_key, **self.kwargs)
+            # keep a copy of the original (full) tree
+            self._full_tree = copy.copy(self.tree)
         else:
             if self.logging:
-                self.logging(f"Input format '{inputformat}' "
-                             f"or mode '{self.mode}' is not valid!")
+                self.logging(f"Input format '{inputformat}' is not valid!")
 
     def write(self, outputfile, outputformat) -> None:
         """Write a tree in JSON or Pickle formats."""
         outputformat = outputformat.lower()
-        if outputformat in self._valid_formats and self.mode in self._valid_modes:
-            write_tree(self.tree, outputfile, outputformat, self.mode, **self.kwargs)
+        if outputformat in self._valid_formats:
+            write_tree(self.tree, outputfile, outputformat, **self.kwargs)
         else:
             if self.logging:
-                self.logging(f"Output format '{outputformat}' "
-                             f"or mode '{self.mode}' is not valid!")
+                self.logging(f"Output format '{outputformat}' is not valid!")
 
-    def filter(self, taxidfilter) -> None:
+    def filter(self, taxidfilter, **kwargs) -> None:
         """Re-build a tree ignoring Taxonomy IDs provided."""
-        message = None
         if not self._full_tree:
             message = ("The Taxonomy Tree needs to be built "
                        "before 'filter' can be called.")
-        if self.mode != "anytree":
-            message = ("Taxonomy Tree filtering is only "
-                       "compatible with mode 'anytree'.")
-        if message:
             if self.logging:
                 logging.warning(message)
             else:
                 print_and_exit(message)
-        self.tree = filter_tree(self.tree, taxidfilter, self.root_key)
+        self.tree = filter_tree(self.tree, taxidfilter, self.root_key, **kwargs)
 
     def validate(self, taxidsearch, taxidfilter=None, **kwargs) -> bool:
         """Validate a list of TaxIDs against a Tree."""
-        if self.mode == "anytree":
-            return validate_tree(self.tree, taxidsearch, taxidfilter, **kwargs)
-        elif self.mode == "fast":
-            return validate_tree_fast(self.tree, taxidsearch, taxidfilter, **kwargs)
+        return validate_tree(self.tree, taxidsearch, taxidfilter, **kwargs)
 
     def validate_by_taxid(self, taxid) -> bool:
         """Validate a TaxIDs against a Tree."""
-        if self.mode == "anytree":
-            return validate_tree_by_taxid(self.tree, taxid)
-        elif self.mode == "fast":
-            return validate_tree_by_taxid_fast(self.tree, taxid)
+        return validate_tree_by_taxid(self.tree, taxid)
 
     def search(self, taxidsearch, taxidfilter=None, **kwargs) -> list or None:
         """Search a Tree based on a list of TaxIds."""
-        message = ("Some of the provided TaxIDs are not valid or not found "
-                   "in the built Tree.")
-        if self.mode == "anytree":
-            if validate_tree(self.tree, taxidsearch, taxidfilter):
-                return search_tree(self.tree, taxidsearch, taxidfilter,
-                                   self.search_rank, **kwargs)
+        if validate_tree(self.tree, taxidsearch, taxidfilter):
+            return search_tree(self.tree, taxidsearch, taxidfilter, **kwargs)
+        else:
+            message = ("Some of the provided TaxIDs are not valid or not found "
+                       "in the built Tree.")
+            if self.logging:
+                logging.warning(message)
             else:
-                if self.logging:
-                    logging.warning(message)
-                else:
-                    print_and_exit(message)
-        elif self.mode == "fast":
-            if validate_tree_fast(self.tree, taxidsearch, taxidfilter):
-                return search_tree_fast(self.tree, taxidsearch, taxidfilter,
-                                        self.search_rank, **kwargs)
-            else:
-                if self.logging:
-                    logging.warning(message)
-                else:
-                    print_and_exit(message)
+                print_and_exit(message)
 
     def search_by_taxid(self, taxid) -> Node or None:
         """Retrieve a node by its unique TaxID."""
-        message = ("The provided TaxIDs is not valid or not found "
-                   "in the built Tree.")
-        if self.mode == "anytree":
-            if validate_tree_by_taxid(self.tree, taxid):
-                return search_tree_by_taxid(self.tree, taxid)
+
+        if validate_tree_by_taxid(self.tree, taxid):
+            return search_tree_by_taxid(self.tree, taxid)
+        else:
+            message = ("The provided TaxIDs is not valid or not found "
+                       "in the built Tree.")
+            if self.logging:
+                logging.warning(message)
             else:
-                if self.logging:
-                    logging.warning(message)
-                else:
-                    print_and_exit(message)
-        elif self.mode == "fast":
-            if validate_tree_by_taxid_fast(self.tree, taxid):
-                return search_tree_by_taxid_fast(self.tree, taxid)
-            else:
-                if self.logging:
-                    logging.warning(message)
-                else:
-                    print_and_exit(message)
+                print_and_exit(message)
+
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -115,13 +115,13 @@ class TestTree:
         resolver.load(os.path.join(cwd, "../testdata/tree.pickle"), "pickle")
         resolver.filter(os.path.join(cwd, "../testdata/taxids_filter.txt"))
         nodes = findall(resolver.tree)
-        assert len(nodes) == 778
+        assert len(nodes) == 1001
         nodes = findall(resolver.tree, filter_=lambda n: n.rank == "species")
         assert len(nodes) == 593
         nodes = [node.name for node in PreOrderIter(resolver.tree)]
-        assert len(nodes) == 778
+        assert len(nodes) == 1001
         nodes = [node.name for node in LevelOrderIter(resolver.tree)]
-        assert len(nodes) == 778
+        assert len(nodes) == 1001
         human = find_by_attr(resolver.tree, "9606")
         assert human.rank == "species"
         assert human.parent.rank == "genus"
@@ -142,13 +142,13 @@ class TestTree:
         resolver = TaxonResolver(logging=context)
         resolver.load(os.path.join(cwd, "../testdata/tree_filtered.json"), "json")
         nodes = findall(resolver.tree)
-        assert len(nodes) == 778
+        assert len(nodes) == 1001
         nodes = findall(resolver.tree, filter_=lambda n: n.rank == "species")
         assert len(nodes) == 593
         nodes = [node.name for node in PreOrderIter(resolver.tree)]
-        assert len(nodes) == 778
+        assert len(nodes) == 1001
         nodes = [node.name for node in LevelOrderIter(resolver.tree)]
-        assert len(nodes) == 778
+        assert len(nodes) == 1001
         human = find_by_attr(resolver.tree, "9606")
         assert human.rank == "species"
         assert human.parent.rank == "genus"
@@ -162,7 +162,7 @@ class TestTree:
         resolver.load(os.path.join(cwd, "../testdata/tree_filtered.pickle"), "pickle")
         tax_ids = resolver.search(os.path.join(cwd, "../testdata/taxids_search.txt"),
                                   os.path.join(cwd, "../testdata/taxids_filter.txt"))
-        assert len(tax_ids) == 302
+        assert len(tax_ids) == 386
 
     def test_resolver_validate(self, context, cwd):
         resolver = TaxonResolver(logging=context)
@@ -193,87 +193,6 @@ class TestTree:
         resolver = TaxonResolver(logging=context)
         # resolver.load(os.path.join(cwd, "../testdata/tree_filtered.json"), "json")
         resolver.load(os.path.join(cwd, "../testdata/tree_filtered.pickle"), "pickle")
-        assert resolver.validate_by_taxid("9606")
-        with pytest.raises(AssertionError):
-            assert resolver.validate_by_taxid("000")
-
-    def test_resolver_build_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        resolver.build(os.path.join(cwd, "../testdata/taxdump.zip"))
-        nodes = resolver.tree["nodes"]
-        assert len(nodes) == 12099
-        human = resolver.search_by_taxid("9606")
-        assert human["parent_tax_id"] == "9605"
-        assert human["rank"] == "species"
-
-    def test_resolver_build_and_write_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        resolver.build(os.path.join(cwd, "../testdata/taxdump.zip"))
-        resolver.write(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        assert os.path.isfile(os.path.join(cwd, "../testdata/tree_fast.pickle"))
-        resolver.write(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        assert os.path.isfile(os.path.join(cwd, "../testdata/tree_fast.json"))
-
-    def test_resolver_load_pickle_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        nodes = resolver.tree["nodes"]
-        assert len(nodes) == 12099
-
-    def test_resolver_load_json_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        nodes = resolver.tree["nodes"]
-        assert len(nodes) == 12099
-
-    def test_resolver_write_and_load_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        resolver.write(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        assert os.path.isfile(os.path.join(cwd, "../testdata/tree_fast.json"))
-
-        resolver = TaxonResolver(mode="fast", logging=context)
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        nodes = resolver.tree["nodes"]
-        assert len(nodes) == 12099
-        human = resolver.search_by_taxid("9606")
-        assert human["parent_tax_id"] == "9605"
-        assert human["rank"] == "species"
-
-    def test_resolver_search_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        # resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        tax_ids = resolver.search(os.path.join(cwd, "../testdata/taxids_search.txt"),
-                                  os.path.join(cwd, "../testdata/taxids_filter.txt"))
-        assert len(tax_ids) == 302
-
-    def test_resolver_validate_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        # resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        assert resolver.validate(os.path.join(cwd, "../testdata/taxids_validate.txt"),
-                                 os.path.join(cwd, "../testdata/taxids_filter.txt"))
-
-    def test_resolver_validate_alt_fast(self, context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        # resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        assert not resolver.validate(os.path.join(cwd, "../testdata/taxids_validate_alt.txt"),
-                                     os.path.join(cwd, "../testdata/taxids_filter.txt"))
-
-    def test_search_by_taxid_fast(self,  context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        # resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
-        human = resolver.search_by_taxid("9606")
-        assert human["parent_tax_id"] == "9605"
-        assert human["rank"] == "species"
-
-    def test_validate_by_taxid_fast(self,  context, cwd):
-        resolver = TaxonResolver(mode="fast", logging=context)
-        # resolver.load(os.path.join(cwd, "../testdata/tree_fast.json"), "json")
-        resolver.load(os.path.join(cwd, "../testdata/tree_fast.pickle"), "pickle")
         assert resolver.validate_by_taxid("9606")
         with pytest.raises(AssertionError):
             assert resolver.validate_by_taxid("000")
